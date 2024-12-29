c80f84645e8d651dc2fdb578249363b74f144b9b
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:10:10 +0800

    Update PRAK002-2410817310014-INDRA-SURYADILAGA.c

--- Soal-2/PRAK002-2410817310014-INDRA-SURYADILAGA.c
+++ Soal-2/PRAK002-2410817310014-INDRA-SURYADILAGA.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("Selamat Pagi, Indra Suryadilaga \nSelamat siang, Indra Suryadilaga \nSelamat malam, Indra Suryadilaga");
+    printf("Selamat Pagi, Indra Suryadilaga \n");
+    printf("Selamat Siang, Indra Suryadilaga \n");
+    printf("Selamat Malam, Indra Suryadilaga");
     return 0;
 }
