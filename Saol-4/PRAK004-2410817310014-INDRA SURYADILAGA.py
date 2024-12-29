3208bac0a91828e34089b8fe87a47610bf569909
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:11:59 +0800

    Update PRAK004-2410817310014-INDRA SURYADILAGA.py

--- Saol-4/PRAK004-2410817310014-INDRA SURYADILAGA.py
+++ Saol-4/PRAK004-2410817310014-INDRA SURYADILAGA.py
@@ -1,12 +1,6 @@
-batas = '=' * (30 + 4)  # Tambah 4 untuk '=' di kedua sisi
-kosong = ' '.center(30)
-nama = 'Indra Suryadilaga'.center(30)
-nim = '2410817310014'.center(30)
-
-print(batas)
-print(f'= {kosong} =')
-print(f'= {nama} =')
-print(f'= {nim} =')
-print(f'= {kosong} =')
-print(batas)
-
+print(f"="*33)
+print(f"={" "*31}=")
+print(f"=\t Indra Suryadilaga \t=")
+print(f"=\t   2410817310014 \t=")
+print(f"={" "*31}=")
+print(f"="*33)
